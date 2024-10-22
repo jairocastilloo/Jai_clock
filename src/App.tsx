0639b2044da6f56631784842c45f045483ce0a50
jaisenpa1 <vajairocastillo@gmail.com>
2024-10-22 19:36:50 +0800

    edit allert

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import { Container, Row, Col } from "react-bootstrap";
 import { IoMdArrowDropupCircle, IoMdArrowDropdownCircle } from "react-icons/io";
